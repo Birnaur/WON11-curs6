826cd0d03f8b84f49a3bb9cb6221e958716f562c
Birnaure <adrianbirnaure@gmail.com>
2023-03-12 18:06:30 +0200

    Homework1 Curs6

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+public class Person{
+    public static void main(String[] args){
+
+    }
+
+    private String name;
+    private int age;
+    private boolean married;
+
+    Person(String name, int age, boolean married){
+        this.name = name;
+        this.age = age;
+        this.married = married;
+        if (married){
+            System.out.println(name + " is " + age + "years old and they are married.");
+        } else {
+            System.out.println(name + " is " + age + "years old and they are not married.");
+        }
+
+    }
+        public String getName(){
+            return name;
+    }
+        public int getAge() {
+            return age;
+    }
+        public boolean isMarried(){
+            return married;
+    }
+}
