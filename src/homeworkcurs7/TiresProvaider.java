f3f9a104a05d7ad43b856796013c6a53ca4cd6ac
Birnaure <adrianbirnaure@gmail.com>
2023-03-14 12:14:01 +0200

    Homework1 Curs7

---
+++ src/homeworkcurs7/TiresProvaider.java
@@ -0,0 +1,11 @@
+package homeworkcurs7;
+
+public interface TiresProvaider {
+    public static void main(String[] args){
+
+    }
+    boolean pay(double amount);
+    String deliverTires();
+    int getDeliveredQuantity();
+}
+
